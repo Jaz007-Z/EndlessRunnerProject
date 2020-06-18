d147069ac947be5551fe6e6eca55e3ae1a9ceb08
Jaz007-Z <65184285+Jaz007-Z@users.noreply.github.com>
2020-06-18 17:00:06 -0400

    Added Viewport/packages
    I added some packages (screens, tool), a test ground texture, and renamed mygds game class to Endless.
    I also added a FitViewport by making a PlayScreen class. Endless also had a game width, height, and PPM variables added to it.

---
+++ core/src/Screens/PlayScreen.java
@@ -0,0 +1,68 @@
+package Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.Endless;
+
+public class PlayScreen implements Screen {
+
+    private Endless game;
+    Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+
+
+    public PlayScreen(Endless game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(Endless.V_WIDTH / Endless.PPM, Endless.V_HEIGHT / Endless.PPM, gamecam);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
