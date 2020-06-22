a7f0a89460fddce09e248ec0ab567c5767fe5772
eliasmoreira <eliasmoreira@pecege.com>
2020-06-22 16:20:47 -0300

    Menu Screen added

--- core/src/com/mygdx/game/Endless.java
+++ core/src/com/mygdx/game/Endless.java
@@ -1,41 +1,40 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import Screens.PlayScreen;
+import com.mygdx.game.screens.GameScreenManager;
+import com.mygdx.game.screens.MenuScreen;
 
-public class Endless extends Game {
+public class Endless extends ApplicationAdapter	 {
 
 	//Virtual Screen size and Box2D Scale(Pixels Per Meter)
-	public static final int V_WIDTH = 400;
-	public static final int V_HEIGHT = 208;
-	public static final float PPM = 100;
+	public static final int V_WIDTH = 700;
+	public static final int V_HEIGHT = 350;
+	public static final String TITLE = "EndlessRunner";
 
-
-
-	public SpriteBatch batch;
-
-	//Texture img;
+	private GameScreenManager gsm;
+	private SpriteBatch batch;
 
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this));
-	} //work
+		gsm = new GameScreenManager();
+		gsm.push(new MenuScreen(gsm));
+	}
 
 	@Override
 	public void render () {
-		super.render();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
+	public void dispose() {
+		super.dispose();
 	}
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.Endless;
 
+import jdk.nashorn.internal.runtime.regexp.joni.ast.EncloseNode;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Endless.V_WIDTH;
+		config.height = Endless.V_HEIGHT;
+		config.title = Endless.TITLE;
 		new LwjglApplication(new Endless(), config);
 	}
 }
