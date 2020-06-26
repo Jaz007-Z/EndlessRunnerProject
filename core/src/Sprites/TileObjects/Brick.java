ce8921c316bed3184d48f5f03a01b5b20633e43d
Nathaniel SNow <64047255+NathanielSnow@users.noreply.github.com>
2020-06-26 14:45:15 -0500

    Add files via upload

---
+++ core/src/Sprites/TileObjects/Brick.java
@@ -0,0 +1,23 @@
+package com.mygdx.game.Sprites.TileObjects;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.Endless;
+import com.mygdx.game.Scenes.Hud;
+import com.mygdx.game.screens.PlayScreen;
+import com.mygdx.game.Sprites.Player;
+
+public class Brick extends InteractiveTileObject {
+    public Brick(PlayScreen screen, MapObject object){
+        super(screen, object);
+        fixture.setUserData(this);
+        setCategoryFilter(Endless.BRICK_BIT);
+    }
+
+    @Override
+    public void onHeadHit(Player player) {
+        Endless.manager.get("audio/sounds/bump.wav", Sound.class).play();
+    }
+
+}
---
+++ core/src/Sprites/TileObjects/Coin.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.Sprites.TileObjects;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.Endless;
+import com.mygdx.game.Scenes.Hud;
+import com.mygdx.game.screens.PlayScreen;
+import com.mygdx.game.Sprites.Items.ItemDef;
+import com.mygdx.game.Sprites.Items.Mushroom;
+import com.mygdx.game.Sprites.Player;
+
+
+public class Coin extends InteractiveTileObject {
+    private static TiledMapTileSet tileSet;
+    private final int BLANK_COIN = 28;
+
+    public Coin(PlayScreen screen, MapObject object){
+        super(screen, object);
+        tileSet = map.getTileSets().getTileSet("tileset_gutter");
+        fixture.setUserData(this);
+        setCategoryFilter(Endless.COIN_BIT);
+    }
+
+    @Override
+    public void onHeadHit(Player player) {
+        if(getCell().getTile().getId() == BLANK_COIN)
+            Endless.manager.get("audio/sounds/bump.wav", Sound.class).play();
+        else {
+            if(object.getProperties().containsKey("mushroom")) {
+                screen.spawnItem(new ItemDef(new Vector2(body.getPosition().x, body.getPosition().y + 16 / Endless.PPM),
+                        Mushroom.class));
+                Endless.manager.get("audio/sounds/powerup_spawn.wav", Sound.class).play();
+            }
+            else
+                Endless.manager.get("audio/sounds/coin.wav", Sound.class).play();
+            getCell().setTile(tileSet.getTile(BLANK_COIN));
+            Hud.addScore(100);
+        }
+    }
+}
