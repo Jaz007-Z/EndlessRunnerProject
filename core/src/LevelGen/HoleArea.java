a99c5fda8b39cca09811d3352bb1f2a424d0e39d
Nathaniel SNow <64047255+NathanielSnow@users.noreply.github.com>
2020-07-19 00:32:13 -0500

    Update HoleArea.java

--- core/src/LevelGen/HoleArea.java
+++ core/src/LevelGen/HoleArea.java
@@ -8,6 +8,11 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.Endless;
 
+/**
+ * @author      Jimmy Zimsky, Dallas Eaton, Elias Moreira, Nathaniel Snow 
+ * @version     1.0                         
+ */
+
 public class HoleArea extends Level {
     public HoleArea(World world) {
         super(world);
